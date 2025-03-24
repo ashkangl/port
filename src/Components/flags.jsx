82b8dbc5e9094128eca77d75fa4589c8a5b22e11
Ashkan Golzad <ashkan72golzad@gmail.com>
2025-03-24 11:46:12 +0330

    fixed

--- src/Components/flags.jsx
+++ src/Components/flags.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 import Iran from '../../public/iran.png'
 import Usa from '../../public/united.png'
 import Uae from '../../public/uae.png'
-import Spain from '../../public/spain.png'
 import Image from 'next/image';
 import Link from 'next/link';
 
 const flags = () => {
   return (
-    <div className='flex flex-row items-center space-x-4 md:w-32 md:ml-48 ml-4'>
+    <div className='flex flex-row items-center justify-around md:w-32 md:ml-48 ml-4'>
         <Link href={`/en`}><Image unoptimized src={Usa} width={25} height={25} alt='Usa Flag' /></Link>
         <Link href={`/fa`} ><Image unoptimized src={Iran} width={25} height={25} alt='Iran Flag' /></Link>
         <Link href={`/ar`} ><Image unoptimized src={Uae} width={25} height={25} alt='Uae Flag' /></Link>
